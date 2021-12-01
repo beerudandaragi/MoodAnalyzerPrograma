005e1995cee24b839d5e5fb531cac1d9d401b42c
beerappa <beerappadandaragi@gmail.com>
2021-12-01 21:20:35 +0530

    Wel Come To The MoodAnalyzer program

---
+++ MoodAnalyzerProgram/MoodAnalyzerProgram/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzerProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WelCome To The MoodAnalyzer Problem");
+            Console.ReadKey();
+        }
+        
+    }
+}
